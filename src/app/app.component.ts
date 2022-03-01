06245ba0c5ddb21fdee2aca4780ed1542832f631
Vikram Bhavsar <vikram.bhavsar@somaiya.edu>
2022-03-01 22:19:08 +0530

    Completed Home Page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Tanaya_Artist_portfolio';
+
+  clicked:boolean = false;
+  @ViewChild("myName") myNameImg !: ElementRef;
+
+
+
+  nameClick(){
+    this.clicked = !this.clicked;
+  }
 }
